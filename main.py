3f2b34d12b496ca641b8ff14e228283b297fea89
Matheus Relchi <matheusrelchicontato@outlook.com.br>
2023-03-24 17:24:23 -0300

    Adding standard patterning functionality to unithal lab

--- main.py
+++ main.py
@@ -1,159 +1,184 @@
 import sys
-import pandas as pd
 from PyQt5.QtWidgets import (
     QApplication,
-    QWidget,
+    QMainWindow,
     QFileDialog,
+    QMessageBox,
+    QAction,
     QTableWidget,
     QTableWidgetItem,
-    QPushButton,
     QVBoxLayout,
-    QInputDialog,
-    QMessageBox,
+    QWidget,
+    QHBoxLayout,
+    QPushButton,
 )
+import pandas as pd
 
 
-class ExcelEditor(QWidget):
+class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.initUI()
 
     def initUI(self):
-        # Define nome da janela
         self.setWindowTitle("LIDIO: Lovingly Improved Data Input Organizer.")
-        self.setGeometry(100, 100, 800, 600)
 
-        # Botão para selecionar o arquivo XLSX
-        self.btn_select_file = QPushButton("Selecionar arquivo", self)
-        self.btn_select_file.clicked.connect(self.select_file)
+        # Criação da barra de menu
+        menubar = self.menuBar()
+        file_menu = menubar.addMenu("Arquivo")
 
-        # Tabela para mostrar os dados do arquivo XLSX
-        self.table_widget = QTableWidget(self)
-        self.table_widget.setRowCount(0)
-        self.table_widget.setColumnCount(0)
+        # Adição do item de abrir arquivo ao menu
+        open_file = QAction("Abrir arquivo", self)
+        open_file.setShortcut("Ctrl+O")
+        open_file.setStatusTip("Abrir arquivo")
+        open_file.triggered.connect(self.openFile)
+        file_menu.addAction(open_file)
 
-        # Botão para salvar o arquivo XLSX com o novo nome de coluna
-        self.btn_save_file = QPushButton("Salvar arquivo", self)
-        self.btn_save_file.clicked.connect(self.save_file)
-        self.btn_save_file.setEnabled(False)
+        # Adição da tabela à janela principal
+        self.tableWidget = QTableWidget()
+        self.setCentralWidget(self.tableWidget)
 
-        # Botão para renomear as colunas
-        self.btn_rename_columns = QPushButton("Renomear colunas", self)
-        self.btn_rename_columns.clicked.connect(self.rename_columns)
-        self.btn_rename_columns.setEnabled(False)
+        # Adição do botão de padronizar dados
+        padronizar_btn = QPushButton("Padronizar dados")
+        padronizar_btn.clicked.connect(self.showOptions)
+        self.padronizar_options = QHBoxLayout()
+        self.padronizar_options.addWidget(padronizar_btn)
 
-        # Adicionando os widgets à janela principal
-        layout = QVBoxLayout(self)
-        layout.addWidget(self.btn_select_file)
-        layout.addWidget(self.table_widget)
-        layout.addWidget(self.btn_rename_columns)
-        layout.addWidget(self.btn_save_file)
+        # Adição do botão de download
+        download_btn = QPushButton("Salvar tabela")
+        download_btn.clicked.connect(self.downloadTable)
+        self.download_layout = QHBoxLayout()
+        self.download_layout.addWidget(download_btn)
 
-    def select_file(self):
-        # Abrir a janela de seleção de arquivo XLSX
-        file_path, _ = QFileDialog.getOpenFileName(
-            self, "Selecionar arquivo", "", "Excel Files (*.xlsx *.xls)"
-        )
+        # Adição do layout vertical para organizar os widgets
+        layout = QVBoxLayout()
+        layout.addLayout(self.padronizar_options)
+        layout.addWidget(self.tableWidget)
+        layout.addLayout(self.download_layout)
 
-        if file_path:
-            # Carregar o arquivo XLSX na tabela
-            df = pd.read_excel(file_path)
-            self.table_widget.setRowCount(len(df.index))
-            self.table_widget.setColumnCount(len(df.columns))
-            self.table_widget.setHorizontalHeaderLabels(df.columns)
+        # Criação do widget principal e definição do layout
+        widget = QWidget()
+        widget.setLayout(layout)
+        self.setCentralWidget(widget)
 
-            for row in range(len(df.index)):
-                for col in range(len(df.columns)):
-                    item = QTableWidgetItem(str(df.iat[row, col]))
-                    self.table_widget.setItem(row, col, item)
+        # Ajuste do tamanho da janela
+        self.resize(800, 600)
+        self.show()
 
-            # Habilitar o botão de salvar
-            self.btn_save_file.setEnabled(True)
-        self.btn_rename_columns.setEnabled(True)
+    def openFile(self):
+        filename, _ = QFileDialog.getOpenFileName(self, "Abrir arquivo", "", "CSV (*.csv);;XLSX (*.xlsx);;XLS (*.xls)")
+        if filename:
+            try:
+                if filename.endswith(".csv"):
+                    self.df = pd.read_csv(filename)
+                elif filename.endswith(".xlsx"):
+                    self.df = pd.read_excel(filename)
+                elif filename.endswith(".xls"):
+                    self.df = pd.read_excel(filename)
+            except Exception as e:
+                QMessageBox.critical(self, "Erro", str(e))
+            else:
+                self.loadTable()
 
-    def save_file(self):
-        # Abrir a janela de seleção de local para salvar o arquivo
-        file_path, _ = QFileDialog.getSaveFileName(
-            self, "Salvar arquivo", "", "Excel Files (*.xlsx *.xls)"
-        )
+    def loadTable(self):
+        self.tableWidget.setRowCount(len(self.df.index))
+        self.tableWidget.setColumnCount(len(self.df.columns))
+        self.tableWidget.setHorizontalHeaderLabels(list(self.df.columns))
 
-        if file_path:
-            # Salvar o arquivo XLSX com as novas colunas renomeadas
-            df = pd.read_excel(self.file_path)
-            new_column_names = [
-                self.table_widget.horizontalHeaderItem(col).text()
-                for col in range(self.table_widget.columnCount())
-            ]
-            df.columns = new_column_names
-            df.to_excel(file_path, index=False)
+        for row in range(len(self.df.index)):
+            for col in range(len(self.df.columns)):
+                self.tableWidget.setItem(row, col, QTableWidgetItem(str(self.df.iloc[row, col]))) 
 
-    def rename_columns(self):
-        num_cols = self.table_widget.columnCount()
+    def showOptions(self):
+        self.padronizar_options.itemAt(0).widget().deleteLater()  # Remove o botão "Padronizar Dados"
+        for i in reversed(range(self.padronizar_options.count())):  # Remove as opções antigas
+            self.padronizar_options.itemAt(i).widget().setParent(None)
 
-        # Cria um diálogo com opções de renomeação
-        options_dialog = QMessageBox(self)
-        options_dialog.setIcon(QMessageBox.Information)
-        options_dialog.setWindowTitle("Opções de Renomeação")
-        options_dialog.setText("Selecione um laboratório:")
-        options_dialog.addButton("Unithal", QMessageBox.AcceptRole)
-        options_dialog.addButton("Lab forte", QMessageBox.AcceptRole)
-        options_dialog.addButton("Agrisolum", QMessageBox.AcceptRole)
+        self.padronizar_options.addWidget(QPushButton("Opção 1"))
+        self.padronizar_options.addWidget(QPushButton("Unithal"))
+        self.padronizar_options.addWidget(QPushButton("Opção 3"))
+        
+        self.padronizar_options.itemAt(1).widget().clicked.connect(self.option1)
 
-        # Exibe o diálogo e obtém a opção selecionada
-        option = options_dialog.exec_()
-        if option == 0:
-            # Renomear para Nome Padrão
-            new_names = [f"Coluna {i}" for i in range(1, num_cols + 1)]
-        elif option == 1:
-            # Renomear com Sufixo
-            suffix, ok = QInputDialog.getText(
-                self,
-                "Renomear colunas",
-                "Digite o sufixo para renomear as colunas:",
-            )
-            if not ok:
-                # O usuário cancelou o diálogo, nada a fazer
-                return
-            new_names = [
-                f"{self.table_widget.horizontalHeaderItem(col).text()}_{suffix}"
-                for col in range(num_cols)
-            ]
-        else:
-            # Renomear Manualmente
-            new_names, ok = QInputDialog.getText(
-                self,
-                "Renomear colunas",
-                "Digite os novos nomes de coluna, separados por vírgula:",
-            )
-            if not ok:
-                # O usuário cancelou o diálogo, nada a fazer
-                return
-            new_names = new_names.split(",")
+    def downloadTable(self):
+        filename, _ = QFileDialog.getSaveFileName(self, "Salvar tabela", "", "XLSX (*.xlsx)")
 
-        if len(new_names) != num_cols:
-            # O número de novos nomes não corresponde ao número de colunas na tabela
-            error_dialog = QMessageBox(self)
-            error_dialog.setIcon(QMessageBox.Warning)
-            error_dialog.setWindowTitle("Erro")
-            error_dialog.setText(
-                f"O número de novos nomes ({len(new_names)}) não corresponde ao número de colunas na tabela ({num_cols})."
-            )
-            error_dialog.exec_()
-            return
+        if filename:
+            try:
+                self.df.to_excel(filename, index=False, engine='xlsxwriter')
+            except Exception as e:
+                QMessageBox.critical(self, "Erro", str(e))
+            else:
+                QMessageBox.information(self, "Sucesso", "Tabela salva com sucesso!")
+                
+    def option1(self):
+        # Renomeando colunas
+        self.df = self.df.rename(columns={'IDENTIF_1': 'id', 'IDENTIF_2': 'prof', 'AL': 'Al', 'AR_FINA': 'Areia fina',
+                        'AR_GROSSA': 'Areia grossa', 'ARGILA': 'Argila', 'CA': 'Ca', 'CA_MG': 'Ca+Mg',
+                        'CL': 'Cl', 'CU': 'Cu', 'FE': 'Fe', 'H_AL': 'H/Al', 'MG': 'Mg', 'MN': 'Mn',
+                        'M_ORG': 'MOS', 'NA': 'Na', 'PH_H2O': 'pH Agua', 'PH_CACL2': 'pH CaCl2',
+                        'PH_SMP': 'ph_smp', 'P_MEL': 'P mehl', 'P_REM': 'prem', 'P_RES': 'P res',
+                        'P_TOTAL': 'P_Total', 'AL_CTC': 'Al%', 'CA_CTC': 'Ca%', 'H_CTC': 'H%',
+                        'MG_CTC': 'Mg%', 'SILTE': 'Silte', 'V': 'V%', 'ZN': 'Zn', 'K_CTC': 'K%'})
+        
+        # Adicionando colunas
+        self.df['AlS'] = ''
+        self.df['Altimetria SRTM'] = ''
+        self.df['Areia total'] = ''
+        self.df['C'] = ''
+        self.df['CaS'] = ''
+        self.df['CEa'] = ''
+        self.df['CO3'] = ''
+        self.df['Ds'] = ''
+        self.df['Fe/Mn'] = ''
+        self.df['FeO'] = ''
+        self.df['H'] = ''
+        self.df['HCO3'] = ''
+        self.df['K mg'] = ''
+        self.df['K/Na'] = ''
+        self.df['KS'] = ''
+        self.df['m%'] = ''
+        self.df['MgS'] = ''
+        self.df['NaS'] = ''
+        self.df['NH4'] = ''
+        self.df['NO3'] = ''
+        self.df['P'] = ''
+        self.df['PB'] = ''
+        self.df['pH'] = ''
+        self.df['ph_kcl'] = ''
+        self.df['PO'] = ''
+        self.df['P/Zn'] = ''
+        self.df['RAS'] = ''
+        self.df['Ca/K'] = ''
+        self.df['Ca/Mg'] = ''
+        self.df['Ca+Mg/K'] = ''
+        self.df['Mg/K'] = ''
+        self.df['H/Al%'] = ''
+        self.df['Na%'] = ''
+        self.df['Si'] = ''
+        self.df['SO4'] = ''
+        self.df['S/P'] = ''
+        self.df['t'] = ''
+        
+        # Padronizando posicoes das colunas
+        self.df = self.df.loc[:, ['id', 'prof', 'Al', 'AlS', 'Altimetria SRTM', 'Areia fina', 'Areia grossa',
+                'Areia total', 'Argila', 'B', 'C', 'Ca', 'Ca+Mg', 'CaS', 'CEa', 'Cl', 'CO3',
+                'CTC', 'Cu', 'Ds', 'Fe', 'Fe/Mn', 'FeO', 'H', 'H/Al', 'HCO3', 'K', 'K mg', 'K/Na',
+                'KS', 'm%', 'Mg', 'MgS', 'Mn', 'MOS', 'N', 'Na', 'NaS', 'NH4', 'NO3', 'P', 'PB', 'pH', 
+                'pH Agua', 'pH CaCl2', 'ph_kcl', 'ph_smp', 'P mehl', 'PO', 'prem', 'P res', 'P_Total',
+                'P/Zn', 'RAS', 'Ca/K', 'Ca/Mg', 'Ca+Mg/K', 'Mg/K', 'S', 'Al%', 'Ca%', 'H%', 'H/Al%',
+                'K%', 'Mg%', 'Na%', 'SB', 'Si', 'Silte', 'SO4', 'S/P', 't', 'V%', 'Zn']]
+        
+        # Removendo items indesejaveis
+        self.df['prof'] = self.df['prof'].str.replace('PONTO ', '')
+        self.df['id'] = self.df['id'].str.replace('(', '', regex=True)
+        self.df['id'] = self.df['id'].str.replace(')', '', regex=True)
 
-        # Renomeia as colunas na tabela
-        for i, new_name in enumerate(new_names):
-            self.table_widget.setHorizontalHeaderItem(i, QTableWidgetItem(new_name))
-
-        # Desabilita o botão de renomear colunas (será habilitado novamente quando o usuário abrir outro arquivo)
-        self.btn_rename_columns.setEnabled(False)
-
-        # Habilita o botão de salvar
-        self.btn_save_file.setEnabled(True)
+        # Atualize a tabela
+        self.loadTable()
 
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    excel_editor = ExcelEditor()
-    excel_editor.show()
+    window = MainWindow()
     sys.exit(app.exec_())
